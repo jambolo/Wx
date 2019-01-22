c1945050d22b0f462fbcea5866a9823fed4630fa
John Bolton <odolvlobo-github@yahoo.com>
2019-01-21 23:42:17 -0800

    Uncrustify pass

--- include/Wx/Wx.h
+++ include/Wx/Wx.h
@@ -3,8 +3,8 @@
 #if !defined(WX_WX_H)
 #define WX_WX_H
 
-#include <windows.h>
 #include <objbase.h>
+#include <windows.h>
 
 #include <Misc/exceptions.h>
 
@@ -12,40 +12,37 @@
 
 namespace Wx
 {
-
-
 //! A windows device context (DC) wrapper
 
 class DeviceContext
 {
 public:
 
-	//! Constructor
-	DeviceContext( HWND hWnd )
-		: m_hWnd( hWnd ), m_hDC( ::GetDC( hWnd ) )
-	{
-	}
+    //! Constructor
+    DeviceContext(HWND hWnd)
+        : m_hWnd(hWnd)
+        , m_hDC(::GetDC(hWnd))
+    {
+    }
 
-	//! Destructor
-	~DeviceContext()
-	{
-		ReleaseDC( m_hWnd, m_hDC );
-	}
+    //! Destructor
+    ~DeviceContext()
+    {
+        ReleaseDC(m_hWnd, m_hDC);
+    }
 
     // non-copyable
     DeviceContext(DeviceContext const &) = delete;
-    DeviceContext & operator=(DeviceContext const &) = delete;
-    
+    DeviceContext & operator =(DeviceContext const &) = delete;
+
     //! Returns the device context handle
-	HDC	GetDC() const		{ return m_hDC; }
+    HDC GetDC() const { return m_hDC; }
 
 private:
 
-	HDC		m_hDC;		//!< Device context handle
-	HWND	m_hWnd;		//!< Window handle
+    HDC m_hDC;          //!< Device context handle
+    HWND m_hWnd;        //!< Window handle
 };
-
-
 
 //! A class that initializes COM while it is in scope
 
@@ -53,22 +50,20 @@ class ComInitializer
 {
 public:
 
-	//! Constructor
-	//
-	//! @param	pvReserved	Use the default value
-	ComInitializer( LPVOID pvReserved = NULL )//, DWORD dwCoInit = COINIT_MULTITHREADED )
-	{
-		CoInitialize( pvReserved );//, dwCoInit );
-	}
+    //! Constructor
+    //
+    //! @param	pvReserved	Use the default value
+    ComInitializer(LPVOID pvReserved = NULL)  // , DWORD dwCoInit = COINIT_MULTITHREADED )
+    {
+        CoInitialize(pvReserved);  // , dwCoInit );
+    }
 
-	// Destructor
-	~ComInitializer()
-	{
-		CoUninitialize();
-	}
+    // Destructor
+    ~ComInitializer()
+    {
+        CoUninitialize();
+    }
 };
-
-
 
 //! An exception to throw when a Win32 function returns a failure
 
@@ -76,121 +71,108 @@ class Win32ErrorException : public std::exception
 {
 public:
 
-	//! Constructor
-	//
-	//! @param	code	The error code (default is S_OK)
-	//! @param	sMsg	The error message (default is "Win32 error reported")
+    //! Constructor
+    //
+    //! @param	code	The error code (default is S_OK)
+    //! @param	sMsg	The error message (default is "Win32 error reported")
 
-	Win32ErrorException( HRESULT code = S_OK, char const * sMsg = "Win32 error reported" ) throw ()
-		: std::exception( sMsg ),
-		m_Code( code )
-	{
-	}
+    Win32ErrorException(HRESULT code = S_OK, char const * sMsg = "Win32 error reported") throw ()
+        : std::exception(sMsg)
+        , m_Code(code)
+    {
+    }
 
-	// Destructor
-	virtual ~Win32ErrorException() throw ()
-	{
-	}
+    // Destructor
+    virtual ~Win32ErrorException() throw ()
+    {
+    }
 
-	//! @name Overrides std::exception
-	//@{
-	//	virtual const char *what() const;
-	//@}
+    //! @name Overrides std::exception
+    //@{
+    //	virtual const char *what() const;
+    //@}
 
-	//! Returns the error code
-	virtual HRESULT	code() const
-	{
-		return m_Code;
-	}
+    //! Returns the error code
+    virtual HRESULT code() const
+    {
+        return m_Code;
+    }
 
 private:
 
-	HRESULT	m_Code;		//!< The error code.
+    HRESULT m_Code;     //!< The error code.
 };
 
+//! Registers a window class. Returns NULL if failed
+ATOM RegisterWindowClass(UINT      style,
+                         WNDPROC   lpfnWndProc,
+                         HINSTANCE hInstance,
+                         LPCTSTR   lpszClassName,
+                         HCURSOR   hCursor       = NULL,
+                         HBRUSH    hbrBackground = NULL,
+                         HICON     hIcon         = NULL,
+                         HICON     hIconSm       = NULL,
+                         LPCTSTR   lpszMenuName  = NULL,
+                         int       cbClsExtra    = 0,
+                         int       cbWndExtra    = 0);
 
+//! Idle function for MessageLoop(). Returns @c true if it should be called again immediately if the queue is empty.
+//
+//! This function is is a parameter to MessageLoop() and is called whenever the message queue is empty. If it
+//! returns @c true, then it will be called again immediately if the message queue is empty. If it returns
+//! @c false, it will not be called until a message has been processed and the queue is empty again.
+//!
+//! @param	hWnd	Window handle
+typedef bool (* MessageLoopIdleCallback)(HWND hWnd);
 
+//! Standard windows message loop. Returns the value in the WM_QUIT message.
+int MessageLoop(HWND hWnd, MessageLoopIdleCallback pCB = 0);
 
-	//! Registers a window class. Returns NULL if failed
-	ATOM RegisterWindowClass(	UINT		style, 
-								WNDPROC		lpfnWndProc, 
-								HINSTANCE	hInstance, 
-								LPCTSTR		lpszClassName,
-								HCURSOR		hCursor			 = NULL, 
-								HBRUSH		hbrBackground	 = NULL, 
-								HICON		hIcon			 = NULL, 
-								HICON		hIconSm			 = NULL,
-								LPCTSTR		lpszMenuName	 = NULL, 
-								int			cbClsExtra		 = 0, 
-								int			cbWndExtra		 = 0 );
+//! @name Debugging Functions
+//!
+//@{
 
+#if defined(_DEBUG)
 
-
-	//! Idle function for MessageLoop(). Returns @c true if it should be called again immediately if the queue is empty.
-	//
-	//! This function is is a parameter to MessageLoop() and is called whenever the message queue is empty. If it
-	//! returns @c true, then it will be called again immediately if the message queue is empty. If it returns
-	//! @c false, it will not be called until a message has been processed and the queue is empty again.
-	//!
-	//! @param	hWnd	Window handle
-	typedef bool ( *MessageLoopIdleCallback )( HWND hWnd );
-
-	//! Standard windows message loop. Returns the value in the WM_QUIT message.
-	int MessageLoop( HWND hWnd, MessageLoopIdleCallback pCB = 0 );
-
-
-
-
-	//! @name Debugging Functions
-	//!
-	//@{
-
-#if defined( _DEBUG )
-
-	//! Dumps the device's caps to the debugger output window
-	void DumpDeviceCaps( HDC hDC );
+//! Dumps the device's caps to the debugger output window
+void DumpDeviceCaps(HDC hDC);
 
 #endif // defined( _DEBUG )
 
-	//! Returns the COM object's current reference count.
-	//
-	//!
-	//! @param	pObject	COM object pointer
+//! Returns the COM object's current reference count.
+//
+//!
+//! @param	pObject	COM object pointer
 
-	template< typename T >
-	inline ULONG GetReferenceCount( T * pObject )
-	{
-		pObject->AddRef();
-		return pObject->Release();
-	}
-	//@}
+template <typename T>
+inline ULONG GetReferenceCount(T * pObject)
+{
+    pObject->AddRef();
+    return pObject->Release();
+}
+//@}
 
+//! @name Other Miscellaneous
+//!
+//@{
 
+//! Releases a COM object and resets the pointer to @c NULL.
+//
+//!
+//! @param	pObject	COM object pointer
 
-	//! @name Other Miscellaneous
-	//!
-	//@{
+template <typename T>
+inline void SafeRelease(T * & pObject)
+{
+    if (pObject != NULL)
+    {
+        pObject->Release();
+        pObject = NULL;
+    }
+}
 
-	//! Releases a COM object and resets the pointer to @c NULL.
-	//
-	//!
-	//! @param	pObject	COM object pointer
-
-	template< typename T >
-	inline void SafeRelease( T * & pObject )
-	{
-		if ( pObject != NULL )
-		{
-			pObject->Release();
-			pObject = NULL;
-		}
-	}
-
-	//@}
-
-
-}; // namespace Wx
-
+//@}
+}  // namespace Wx
 
 //! Asserts that the HRESULT value indicates success.
 //
@@ -199,15 +181,17 @@ private:
 //!
 //! @hideinitializer
 
-#if defined( _DEBUG )
+#if defined(_DEBUG)
 
 #include <Misc/Assert.h>
 
-#define assert_succeeded( hr )	(void)( SUCCEEDED( hr ) || ( non_standard_assert_display( #hr " indicates failure.", __FILE__, __LINE__ ), 0 ) )
+#define assert_succeeded(hr) \
+    (void)(SUCCEEDED(hr) ||  \
+           (non_standard_assert_display( #hr " indicates failure.", __FILE__, __LINE__), 0))
 
 #else // defined ( _DEBUG )
 
-#define assert_succeeded( hr )	((void)0)
+#define assert_succeeded(hr) ((void)0)
 
 #endif // defined( _DEBUG )
 
